6fe0e74b45f6b949c3417a6d1f3defafdd463c03
Leandro Silva <ldfsilva@gmail.com>
2017-09-11 16:50:35 -0500

    Rename README file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 import os
 
 
-README = os.path.join(os.path.dirname(__file__), 'README.md')
+README = os.path.join(os.path.dirname(__file__), 'README.rst')
 REQUIREMENTS = os.path.join(os.path.dirname(__file__), 'requirements.txt')
 
 
