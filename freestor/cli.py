282750e75725eff41e3ff8a945db7b174ede3c44
Leandro Silva <ldfsilva@gmail.com>
2017-09-14 14:11:57 -0500

    Add functions to output data in CSV and JSON format

--- freestor/cli.py
+++ freestor/cli.py
@@ -1,9 +1,31 @@
 """Command line interface."""
+import sys
+import csv
+import json
 import argparse
 
 from getpass import getpass
 
 from freestor import FreeStor
+
+
+def f_csv(data):
+    """Output data in CSV format"""
+
+    # extract header from first object
+    header = list(data[0].keys())
+    # output data to standard output
+    output = sys.stdout
+    writer = csv.DictWriter(output, fieldnames=header)
+    writer.writeheader()
+    for device in data:
+        writer.writerow(device)
+
+
+def f_json(data):
+    """Output data in JSON format"""
+
+    print(json.dumps(data, indent=4))
 
 
 def main():
@@ -17,8 +39,11 @@ def main():
 
     parser.add_argument('--get-pdevs', action='store_true', help='Get all physical disk devices information')
 
+    parser.add_argument('--json', help='Output data in JSON format, default is CSV.', action='store_const', dest='output', const=f_json, default=f_csv)
+
     args = parser.parse_args()
 
+    output = args.output
     password = args.password or getpass("Provide %s's password: " % args.username)
 
     freestor = FreeStor(args.server, args.username, password)
@@ -28,4 +53,4 @@ def main():
     if args.get_pdevs:
         data = freestor.get_pdevs()
         print()
-        print(data)
+        output(data)
