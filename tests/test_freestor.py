9fef927b3cf1b4afcea3df3dbd1b176e4fb8bbc5
Leandro Silva <ldfsilva@gmail.com>
2017-10-03 14:37:08 -0500

    Fix tests which were broken due to FreeStor class implementation

--- tests/test_freestor.py
+++ tests/test_freestor.py
@@ -1,8 +1,7 @@
 import unittest
 import json
-from os import getenv
 from unittest.mock import patch
-from freestor import *
+from freestor import FreeStor
 
 
 def load_json(file_name):
@@ -14,22 +13,18 @@ def load_json(file_name):
 
 
 class TestFreestor(unittest.TestCase):
-    @patch('freestor.requests.post')
-    def setUp(self, mock_get):
+
+    @patch('freestor.FreeStor._post')
+    def setUp(self, mock_post):
         expected_json = {'rc': 0, 'type': 'root',
                         'id': 'b5588eea-0354-46db-8934-5504204ad183'}
-        mock_get.return_value.json.return_value = expected_json
 
-        #Get environment variables for real test
-        self.server = getenv('CDP_SERVER')
-        self.username = getenv('CDP_USERNAME', 'root')
-        self.password = getenv('CDP_PASSWORD', 'IPStor101')
-        self.session_id = get_session_id(self.server, 
-                                        self.username, 
-                                        self.password)
+        mock_post.return_value._post.return_value = expected_json
 
-    @patch('freestor.requests.post')
-    def test_get_session_id_ok(self, mock_get):
+        self.cdp = FreeStor('dagcdp01', 'root', 'abc')
+
+    @patch('freestor.FreeStor._post')
+    def test_get_session_id_ok(self, mock_post):
         """
         A successful request must return a hash id 36 chars long.
         """
@@ -37,15 +32,14 @@ class TestFreestor(unittest.TestCase):
         #Expected json response
         expected_json = {'rc': 0, 'type': 'root',
                         'id': 'b5588eea-0354-46db-8934-5504204ad183'}
-        mock_get.return_value.json.return_value = expected_json
+        # mock_get.return_value.json.return_value = expected_json
+        mock_post.return_value = expected_json
 
-        session_id = get_session_id(self.server, 
-                                    self.username, 
-                                    self.password)
+        session_id = self.cdp.get_session_id()
 
         self.assertEqual(36, len(session_id))
 
-    @patch('freestor.requests.get')
+    @patch('freestor.FreeStor._get')
     def test_get_fc_adapters(self, mock_get):
         """
         A list of all fc adapters ID must be returned.
@@ -54,14 +48,15 @@ class TestFreestor(unittest.TestCase):
         #Expected json response
         f_name = 'tests/fc_adapters.json'
         expected_json = load_json(f_name)
-        mock_get.return_value.json.return_value = expected_json
+
+        mock_get.return_value = expected_json
 
         expected = [100, 101, 102, 103]
-        hbas = get_fc_adapters(self.server, self.session_id)
+        hbas = self.cdp.get_fc_adapters()
 
         self.assertListEqual(expected, hbas)
 
-    @patch('freestor.requests.get')
+    @patch('freestor.FreeStor._get')
     def test_get_fc_detail_mode_initiator(self, mock_get):
         """
         When fc adapter is set to initiator mode, it has to 
@@ -70,15 +65,16 @@ class TestFreestor(unittest.TestCase):
 
         f_name = 'tests/fc_adapter_100_detail.json'
         expected_json = load_json(f_name)
-        mock_get.return_value.json.return_value = expected_json
+
+        mock_get.return_value = expected_json
 
         expected = [['FC Adapter 100', 'QLogic', 'initiator', 'linkdown',
                     '21:00:00:e0:8b:94:30:05', 'initiator'],]
-        adapter = get_fc_detail(self.server, self.session_id, 101)
+        adapter = self.cdp.get_fc_detail(101)
 
         self.assertListEqual(expected, adapter)
 
-    @patch('freestor.requests.get')
+    @patch('freestor.FreeStor._get')
     def test_get_fc_detail_dual_mode(self, mock_get):
         """
         When fc adapter is set to dual mode, it has to 
@@ -88,15 +84,14 @@ class TestFreestor(unittest.TestCase):
 
         f_name = 'tests/fc_adapter_101_detail.json'
         expected_json = load_json(f_name)
-        mock_get.return_value.json.return_value = expected_json
- 
+        mock_get.return_value = expected_json
+
         expected = [
             ['FC Adapter 101', 'QLogic', 'dual', 'linkdown',
             '21:01:00:e0:8b:b4:30:05', 'initiator'],
             ['FC Adapter 101', 'QLogic', 'dual', 'linkdown',
             '21:01:00:0d:77:b4:30:05', 'target']
         ]
-        adapter = get_fc_detail(self.server, self.session_id, 101)
- 
-        self.assertListEqual(expected, adapter)
+        adapter = self.cdp.get_fc_detail(101)
 
+        self.assertListEqual(expected, adapter)
